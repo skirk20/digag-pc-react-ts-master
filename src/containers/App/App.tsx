5d28a47c3e5e163455ed13aa19dd1430f05305ec
skirk20 <lukasjanszek@gmail.com>
2022-11-07 15:58:00 +0100

    first commit

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './App.css';
+import Header from "../Header/Header";
+class App extends React.Component {
+    render() {
+        return (
+            
+            <body>
+                <div>
+                    <Header/>
+                </div>
+                <div>hello</div>    
+            </body>
+            
+        );
+    }
+}
+
+export default App;
---
+++ src/containers/Header/Header.tsx
@@ -0,0 +1,17 @@
+/**
+ * Created by Yuicon on 2017/10/21.
+ */
+import * as React from 'react';
+// import {Menu, Icon, Dropdown, message} from 'antd';
+import './Header.css';
+
+class Header extends React.Component {
+    render(){
+        return(
+            <p>pusto</p>
+        )
+    }
+    
+}
+
+export default Header;
